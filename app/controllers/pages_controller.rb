b91d400c018dff819851d0ec81de4c35a9249d05
Paolo Cosentino <cosepaol88@gmail.com>
2015-05-04 15:18:18 -0500

    user mailer

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,13 @@ class PagesController < ApplicationController
 
      def gallary
      end
+
+     def send_email
+          user_name =  "#{params[:user][:first_name]} #{params[:user][:last_name]}"
+          email_address = params[:user][:email_address]
+          notes = params[:user][:notes]
+          UserMailer.send_email({:name => user_name, :email => email_address, :description => notes}).deliver
+          respond_to :js
+     end
+     
 end
