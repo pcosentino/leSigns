827aa5d1c8857737bbf87152dcc97e490c72e392
Paolo Cosentino <cosepaol88@gmail.com>
2015-05-04 16:15:39 -0500

    test

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,11 +1,11 @@
 class UserMailer < ActionMailer::Base
-  default from: "pcosentino88@gmail.com"
+  default from: "peterlibreri@gmail.com"
 
   def send_email(user_info)
     @name = user_info[:name]
     @email = user_info[:email]
     @description = user_info[:description]
-    mail(:to => "pcosentino88@gmail.com", :subject => "Portfolio Notification")
+    mail(:to => "peterlibreri@gmail.com", :subject => "Portfolio Notification")
   end
 
 
