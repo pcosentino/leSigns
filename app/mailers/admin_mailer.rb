ad72380d45c4387fbf76a6736a2f1a8277e5e6bf
Paolo Cosentino <cosepaol88@gmail.com>
2015-05-09 11:15:56 -0500

    new email

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,12 +1,12 @@
 class AdminMailer < ActionMailer::Base
-  default from: "pcosentino88@gmail.com"
+  default from: "cosepaol88@gmail.com"
 
 
     def send_email(user_info)
     @name = user_info[:name]
     @email = user_info[:email]
     @description = user_info[:description]
-    mail(:to => "pcosentino88@gmail.com", :subject => "Portfolio Notification")
+    mail(:to => "cosepaol88@gmail.com", :subject => "Portfolio Notification")
   end
 
 end
