7fd4391751d4e95831e9c8847c2a350182fa5349
Paolo Cosentino <cosepaol88@gmail.com>
2015-05-09 11:29:13 -0500

    update email2

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,11 +1,11 @@
 class UserMailer < ActionMailer::Base
-  default from: "pcosentino88@gmail.com"
+  default from: "cosepaol88@gmail.com"
 
   def send_email(user_info)
     @name = user_info[:name]
     @email = user_info[:email]
     @description = user_info[:description]
-    mail(:to => "pcosentino88@gmail.com", :subject => "Portfolio Notification")
+    mail(:to => "cosepaol88@gmail.com", :subject => "Portfolio Notification")
   end
 
 
